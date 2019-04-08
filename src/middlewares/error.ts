804b21810e6a72cbc8b15c6a5e3fa463cb9f76bb
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-08 16:27:28 +0200

    conditional logging.

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,11 +1,14 @@
 import { ParameterizedContext } from 'koa';
 
-export default async (ctx: ParameterizedContext, next: any) => {
+export default (log: boolean = true) => async (
+  ctx: ParameterizedContext,
+  next: any,
+) => {
   try {
     await next();
   } catch (err) {
     ctx.status = 500;
     ctx.body = { msg: 'Something had gone wrong, we are working on it' };
-    console.log(err);
+    if (log) { console.log(err); }
   }
 };
