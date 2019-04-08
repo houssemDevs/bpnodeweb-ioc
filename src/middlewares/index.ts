0e1bed8a6faa2f26afde0d1655461dd513fce915
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-08 16:48:32 +0200

    log errors to console.

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -5,6 +5,6 @@ import favicon from './favicon';
 import id from './id';
 import logger from './logger';
 
-const middlewares = koacompose([favicon, id, logger, error]);
+const middlewares = koacompose([favicon, id, logger, error(true)]);
 
 export default middlewares;
