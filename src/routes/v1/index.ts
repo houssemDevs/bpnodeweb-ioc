4d4ea6ae1065c044edb71db4d794da65dfe50b89
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-08 16:46:48 +0200

    cleaning routes.

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,11 +2,4 @@ import koarouter from 'koa-router';
 
 const router = new koarouter({ prefix: '/v1' });
 
-router.get('/', async (ctx) => {
-  throw new Error('fuck it on purpose');
-  ctx.status = 200;
-  ctx.type = 'application/json';
-  ctx.body = { msg: 'boilerplate - simple node web service' };
-});
-
 export default router;
