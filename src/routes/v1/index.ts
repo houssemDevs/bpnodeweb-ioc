d6e96cfcc652f3e9afd2af55ea1d239611b4d2b9
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-08 10:44:12 +0200

    try to throw test the middleware.

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,7 +2,8 @@ import koarouter from 'koa-router';
 
 const router = new koarouter({ prefix: '/v1' });
 
-router.get('/', async ctx => {
+router.get('/', async (ctx) => {
+  throw new Error('fuck it on purpose');
   ctx.status = 200;
   ctx.type = 'application/json';
   ctx.body = { msg: 'boilerplate - simple node web service' };
