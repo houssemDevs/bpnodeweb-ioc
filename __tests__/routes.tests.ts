abc8b13dd8239e07695ec82fe9e8fd2f00e01c34
Houssem Ghiat <h.ghiat@ctcdre.loc>
2019-04-08 16:27:15 +0200

    testing routes.

--- __tests__/routes.tests.ts
+++ __tests__/routes.tests.ts
@@ -6,9 +6,7 @@ describe('routes', () => {
   describe('/', () => {
     it('should respond correctly', async () => {
       const resp = await request(app.callback()).get('/v1');
-      expect(resp.status).toBe(200);
-      expect(resp.body).toEqual({ msg: 'boilerplate - simple node web service' });
-      expect(resp.type).toBe('application/json');
+      expect(resp.status).toBe(404);
     });
   });
 });
